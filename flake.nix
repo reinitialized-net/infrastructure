7f1c3bfe1f1dc526d47409ea51a18fbd87f80ac1
Reinitialized <me@reinitialized.studio>
2025-08-31 16:26:35 -0500

    corrected nixpkgs.url

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A Nix flake for building and managing NixOS systems";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
   };
 
   outputs = { self, nixpkgs, ... }:
