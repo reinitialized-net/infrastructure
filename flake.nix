27849648480ec2edd1b4ef1e68f98ac48145357f
Reinitialized <me@reinitialized.studio>
2025-08-31 20:18:20 -0500

    idk whats going on?!?

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "A Nix flake for building and managing NixOS systems";
+  description = "Official Bleu Pigger NixOS Flake";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
@@ -8,13 +8,17 @@
   outputs = { self, nixpkgs, ... }:
     let
       system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
+      pkgs = import nixpkgs { 
+        inherit system;
+      };
       nixosSystem = nixpkgs.lib.nixosSystem;
     in
     {
       packages.${system}.iso = nixosSystem {
         inherit system;
-        modules = [ ./nixos/demo.nix ];
+        modules = [ 
+          ./nixos/demo.nix
+        ];
       }.config.system.build.isoImage;
 
       nixosConfigurations.nixos-demo = nixosSystem {
