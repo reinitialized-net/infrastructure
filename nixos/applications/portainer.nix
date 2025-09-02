0826353c2f5ef6966060eeeaa9a44538f811e169
Reinitialized <me@reinitialized.studio>
2025-09-01 22:13:36 -0500

    confirming setting podman.user causes issues

--- nixos/applications/portainer.nix
+++ nixos/applications/portainer.nix
@@ -2,25 +2,6 @@
 
 {
   # 1) Deploy Portainer using oci-container interface
-  # systemd.services.portainer = {
-  #   description = "Portainer container management UI (Podman)";
-  #   after = [ "network.target" "podman.socket" ];
-  #   wantedBy = [ "multi-user.target" ];
-  #   serviceConfig = {
-  #     Type = "simple";
-  #     User = "podman";
-  #     ExecStart = ''
-  #       ${pkgs.podman}/bin/podman run \
-  #         --name portainer \
-  #         -p 9000:9000 \
-  #         -v portainer_data:/data \
-  #         -v /run/podman/podman.sock:/var/run/docker.sock \
-  #         docker.io/portainer/portainer-ce:latest
-  #     '';
-  #     ExecStop = "${pkgs.podman}/bin/podman stop portainer";
-  #     Restart = "always";
-  #   };
-  # };
   virtualisation.oci-containers.containers.portainer = {
     image = "docker.io/portainer/portainer-ce:latest";
     ports = [ "9000:9000" ];
@@ -32,7 +13,7 @@
     autoStart = true;
     serviceName = "portainer";
 
-    #podman.user = lib.mkIf (config.virtualisation.podman.enable) "podman";
+    podman.user = lib.mkIf (config.virtualisation.podman.enable) "podman";
   };
 
   # Allow firewall access to Portainer (port 9000)
