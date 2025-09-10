ff251d2c3bd22eed9eff5ae227df8195af89663c
Reinitialized <me@reinitialized.studio>
2025-09-09 19:27:43 -0500

    refix cause i now understand what i did

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
   };
 
-  outputs = { self, nixpkgs, ... }:
+  outputs = { self, nixpkgs, ... }@inputs:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { 
