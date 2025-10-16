23ab7ef56784dbed316d0f059af6a571b66d19d0
Reinitialized <me@reinitialized.studio>
2025-10-16 01:08:11 -0500

    created develop user

--- flake.nix
+++ flake.nix
@@ -41,6 +41,28 @@
 
               nixd
             ];
+
+            # Create develop user
+            fileSystems."/home/develop" = {
+              device = "/dev/disk/by-id/scsi-0QEMU_QEMU_HARDDISK_drive-scsi2";
+              fsType = "ext4";
+              options = [ "defaults" ];
+
+              autoResize = true;
+              autoFormat = true;
+            };
+            users.users.develop = {
+              extraGroups = [ "docker" "wheel" ];
+              shell = pkgs.bashInteractive;
+
+              isNormalUser = true;
+              home = "/home/develop";
+              initialPassword = "!";
+
+              openssh.authorizedKeys.keys = [
+                "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEgNNIkOFenuf9S6sy5heFeysErwMgfGD//r4jWgbg/E develop"
+              ];
+            };
           }
         ];
       };
