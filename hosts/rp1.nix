3f5046767a9a4532f1d12bae7609403d012c92b3
Reinitialized <me@reinitialized.studio>
2025-10-19 15:38:39 -0500

    cleanup and fixes

--- hosts/rp1.nix
+++ hosts/rp1.nix
@@ -1,31 +1,37 @@
 # hosts/rp1.nix
 ## 
-{ pkgs, ...}:
+{ ... }:
 {
   imports = [
     ../hardware/qemu.nix
-    
     ../modules/standard.nix
   ];
 
   # System-specific configuration
-  networking.hostName = "devenv";
-  networking.interfaces.eth0.ipv4.addresses = [
-    {
-      address = "10.1.12.2";
-      prefixLength = 29;
-    }
-    {
-      address = "10.1.12.3";
-      prefixLength = 29;
-    }
-        {
-      address = "10.1.12.4";
-      prefixLength = 29;
-    }
-  ];
-  networking.defaultGateway = "10.1.12.1";
-  networking.nameservers = [ "1.1.1.1" "8.8.8.8" ];
+  networking = {
+    hostName = "rp1";
+    interfaces.eth0.ipv4.addresses = [
+      {
+        address = "10.1.12.2";
+        prefixLength = 29;
+      }
+      {
+        address = "10.1.12.3";
+        prefixLength = 29;
+      }
+      {
+        address = "10.1.12.4";
+        prefixLength = 29;
+      }
+    ];
+    defaultGateway = "10.1.12.1";
+    nameservers = [ 
+      "1.1.1.1"
+      "8.8.8.8" 
+    ];
+    firewall.allowedTCPPorts = [ 80 443 ];
+    firewall.allowedUDPPorts = [ 80 443 ];
+  };
 
   # Setup Caddy container
   containers.caddy = {
