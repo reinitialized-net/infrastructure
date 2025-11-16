660c81ce4be60b423c651308af969c78634b7f81
Reinitialized <me@reinitialized.studio>
2025-11-15 23:33:28 -0600

    extensions/firewall.nix: - add support for ipv4/ipv6

--- extensions/firewall.nix
+++ extensions/firewall.nix
@@ -17,7 +17,12 @@ let
         description = "The protocol for the port.";
         default = "tcp";
       };
-      sourceAddresses = mkOption {
+      ipType = mkOption {
+        type = types.enum [ "ipv4" "ipv6" ];
+        description = "The IP type for the rule.";
+        default = "ipv4";
+      };
+      source = mkOption {
         type = types.listOf types.str;
         description = "List of source IP addresses or CIDR blocks allowed to access the port.";
         default = [ "0.0.0.0/0" ];
@@ -37,21 +42,18 @@ in
     lib.mkMerge [
       (lib.mkIf (config.networking.firewall.package == "nftables") {
         # Actions for nftables
-        networking.nftables.extraRules = lib.concatMapStringsSep "\n"
-          (entry: ''
-            table inet filter {
-              chain input {
-                type filter hook input priority 0;
-                ${lib.concatMapStringsSep "\n" (addr: "ip saddr ${addr} ${entry.protocol} dport ${toString entry.port} accept") entry.sourceAddresses}
-              }
-            }
-          '') config.networking.firewall.whitelist;
+        networking.firewall.extraInputRules = lib.concatMapStringsSep "\n"
+          (entry: lib.concatMapStringsSep "\n"
+            (addr: let ipPrefix = if entry.ipType == "ipv4" then "ip saddr" else "ip6 saddr"; in "${ipPrefix} ${addr} ${entry.protocol} dport ${toString entry.port} accept")
+            entry.source
+          )
+          config.networking.firewall.whitelist;
       })
       (lib.mkIf (config.networking.firewall.package == "iptables") {
         # Actions for iptables
-        networking.firewall.extraInputRules = lib.concatMapStringsSep "\n"
+        networking.firewall.extraCommands = lib.concatMapStringsSep "\n"
           (entry: lib.concatMapStringsSep "\n"
-            (addr: " -A INPUT -p ${entry.protocol} -s ${addr} --dport ${toString entry.port} -j ACCEPT")
+            (addr: let ipPrefix = if entry.ipType == "ipv4" then "-4" else "-6"; in " -A INPUT ${ipPrefix} -p ${entry.protocol} -s ${addr} --dport ${toString entry.port} -j ACCEPT")
             entry.sourceAddresses
           )
           config.networking.firewall.whitelist;
