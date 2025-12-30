be64f44693e6fa3107ff20de7c8e6ce725aa19e6
Reinitialized <me@reinitialized.studio>
2025-12-30 12:26:25 -0600

    Add initial makeConfiguration.nix file for system configuration

---
+++ library/makeConfiguration.nix
@@ -0,0 +1,25 @@
+{
+  defaultStateVersion,
+  self,
+  nixpkgs ? self.inputs.nixpkgsStable,
+}: host: {
+  modules ? [],
+  system ? "x86_64-linux",
+  hardware ? "qemu",
+}: nixpkgs.lib.nixosSystem {
+  inherit system;
+  
+  specialArgs = {
+    inherit self
+      system;
+    inherit (nixpkgs) lib;
+  };
+  modules = modules ++ [
+    "${self}/modules/hardware/${hardware}.nix"
+    "${self}/modules/profiles/standard.nix"
+    (if host == "standard" then {} else "${self}/modules/hosts/${host}.nix")
+    {
+      system.stateVersion = nixpkgs.lib.mkDefault defaultStateVersion;
+    }
+  ];
+}
