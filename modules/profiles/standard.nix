5d65c09b8e224995a6c6b93688fbe5e9f1b8a2bd
Reinitialized <me@reinitialized.studio>
2026-01-19 17:50:01 -0600

    Disable root login by default, unless overriden

--- modules/profiles/standard.nix
+++ modules/profiles/standard.nix
@@ -46,7 +46,7 @@
 
     users = {
       root = {
-        initialPassword = lib.mkForce "Abcd#1234!"; # temporary credential for debugging
+        initialHashedPassword = lib.mkForce null;
         shell = lib.mkForce pkgs.bashInteractive;
       };
       rnetadmin = {
