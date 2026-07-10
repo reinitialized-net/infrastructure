6e81405f90d95d015f391fd35d9346ca9bd663bb
Infratainer <infratainer@reinitialized.net>
2026-07-10 01:06:40 -0500

    chore(deps): update ghcr.io/matrix-construct/tuwunel docker tag to v1.8.1

--- hosts/apps3.nix
+++ hosts/apps3.nix
@@ -110,7 +110,7 @@
     tuwunel = {
       autoStart = true;
       hostname = "tuwunel";
-      image = "ghcr.io/matrix-construct/tuwunel:v1.8.0";
+      image = "ghcr.io/matrix-construct/tuwunel:v1.8.1";
       environment = config.secrets.tuwunel.keys;
       networks = [
         "backend"
