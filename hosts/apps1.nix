809889b564e640a67b56ae949dccf83016b09e04
Infratainer <infratainer@reinitialized.net>
2026-09-05 01:11:20 -0500

    chore(deps): update hududocker/hudu docker tag to v2.45.1

--- hosts/apps1.nix
+++ hosts/apps1.nix
@@ -83,7 +83,7 @@
     hudu1 = {
       autoStart = true;
       hostname = "hudu1";
-      image = "hududocker/hudu:2.45.0";
+      image = "hududocker/hudu:2.45.1";
       environment = config.secrets.hudu.keys;
       networks = [
         "backend"
@@ -100,7 +100,7 @@
     hudu2 = {
       autoStart = true;
       hostname = "hudu2";
-      image = "hududocker/hudu:2.45.0";
+      image = "hududocker/hudu:2.45.1";
       environment = config.secrets.hudu.keys;
       cmd = [
         "bundle"
